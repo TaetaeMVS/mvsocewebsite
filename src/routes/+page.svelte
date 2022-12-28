392a4732f5bb22b907d6819a0930f548cafc44d7
Tae Yeong Kim <taeyeong200@gmail.com>
2022-12-28 22:56:42 +1100

    add background image, nav bar, hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,55 @@
-<header class="bg-blue-500 text-white p-4 shadow-md">
-  <h1 class="text-2xl font-bold">Welcome to Multiversus Oce</h1>
-</header>
-<main class="container mx-auto p-4">
-  <p class="text-xl font-semibold mb-4">
-    <a href="https://discord.gg/mvsoce" class="text-blue-500 hover:text-blue-700">
-      <i class="fab fa-discord"></i> Visit our Discord
-    </a> |
-    <a href="https://twitter.com/MultiVersus_OCE" class="text-blue-500 hover:text-blue-700">
-      <i class="fab fa-twitter"></i> Follow us on Twitter
-    </a> |
-    <a href="https://twitch.tv/multiversus_oce" class="text-blue-500 hover:text-blue-700">
-      <i class="fab fa-twitch"></i> Watch us on Twitch
-    </a>
-  </p>
-  <button class="btn btn-blue-500">Join us on Discord</button>
-</main>
+<style>
+.navbar {
+  position: fixed; /* Fixes the navbar to the top of the page */
+  top: 0; /* Positions the navbar at the top of the page */
+  left: 0; /* Positions the navbar at the left side of the page */
+  right: 0; /* Takes up the full width of the page */
+  z-index: 1; /* Ensures that the navbar is displayed above other elements on the page */
+  background-color: transparent;
+}
+a.top-left {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 2;
+  width: 10%;
+  height: 10%;
+}
+</style>
+
+
+
+<div class="hero min-h-screen" style="background-image: url('../spacejam.jpg');">
+  <div class="hero-overlay bg-opacity-0"></div>
+  <div class="hero-content text-center text-neutral-content">
+    <div class="navbar bg-base-100">
+      <div class="flex-1">
+        <a class="btn btn-ghost normal-case text-xl top-left">
+          <img src="../multiversusocetransparent.png" alt="Multiversus OCE logo">
+        </a>        
+      </div>
+      <div class="flex-none">
+        <ul class="menu menu-horizontal px-1">
+          <li><a>Item 1</a></li>
+          <li tabindex="0">
+            <a>
+              Parent
+              <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+            </a>
+            <ul class="p-2 bg-base-100">
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </li>
+          <li><a>Item 3</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">Join Multiversus OCE</h1>
+      <p class="mb-5"></p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
