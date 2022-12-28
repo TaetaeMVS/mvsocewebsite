990da24b4348ffd68d663ff7da1d5af8cab062a1
Tae Yeong Kim <taeyeong200@gmail.com>
2022-12-28 17:36:21 +1100

    added tailwind css and daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,17 @@
-<h1>Welcome to Multiversus Oce</h1>
-<p>
-  <a href="https://discord.gg/mvsoce"><i class="fab fa-discord"></i> Visit our Discord</a> |
-  <a href="https://twitter.com/MultiVersus_OCE"><i class="fab fa-twitter"></i> Follow us on Twitter</a> |
-  <a href="https://twitch.tv/multiversus_oce"><i class="fab fa-twitch"></i> Watch us on Twitch</a>
-</p>
+<header class="bg-blue-500 text-white p-4 shadow-md">
+  <h1 class="text-2xl font-bold">Welcome to Multiversus Oce</h1>
+</header>
+<main class="container mx-auto p-4">
+  <p class="text-xl font-semibold mb-4">
+    <a href="https://discord.gg/mvsoce" class="text-blue-500 hover:text-blue-700">
+      <i class="fab fa-discord"></i> Visit our Discord
+    </a> |
+    <a href="https://twitter.com/MultiVersus_OCE" class="text-blue-500 hover:text-blue-700">
+      <i class="fab fa-twitter"></i> Follow us on Twitter
+    </a> |
+    <a href="https://twitch.tv/multiversus_oce" class="text-blue-500 hover:text-blue-700">
+      <i class="fab fa-twitch"></i> Watch us on Twitch
+    </a>
+  </p>
+  <button class="btn btn-blue-500">Join us on Discord</button>
+</main>
