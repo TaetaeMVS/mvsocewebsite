acf36bdb6f98a6d32c9bd9527325f7e607338103
Tae Yeong Kim <taeyeong200@gmail.com>
2022-12-28 15:55:15 +1100

    add socials

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
 <h1>Welcome to Multiversus Oce</h1>
-<p><a href="https://discord.gg/mvsoce">Visit our Discord</a></p>
+<p>
+  <a href="https://discord.gg/mvsoce"><i class="fab fa-discord"></i> Visit our Discord</a> |
+  <a href="https://twitter.com/MultiVersus_OCE"><i class="fab fa-twitter"></i> Follow us on Twitter</a> |
+  <a href="https://twitch.tv/multiversus_oce"><i class="fab fa-twitch"></i> Watch us on Twitch</a>
+</p>
