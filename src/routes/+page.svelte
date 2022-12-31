f0a749cb48eabb61f46dce451d03d9f1cc704e3b
Eamon Heffernan <eamon.heffernan@finplex.com.au>
2022-12-31 16:12:09 +1100

    console.logs for testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,19 @@
 	import { supabase } from '$lib/supabaseClient';
 
 	const getURL = () => {
+		console.log(import.meta.env.VITE_SITE_URL);
+		console.log(import.meta.env.VITE_VERCEL_URL);
+
 		let url =
 			import.meta.env.VITE_SITE_URL ?? // Set this to your site URL in production env.
 			import.meta.env.VITE_VERCEL_URL ?? // Automatically set by Vercel.
-			'http://localhost:5173/';
+			'http://localhost:4173/';
 		// Make sure to include `https://` when not localhost.
 		url = url.includes('http') ? url : `https://${url}`;
 		// Make sure to including trailing `/`.
 		url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+
+		console.log(url);
 		return url;
 	};
 
