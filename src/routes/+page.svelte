ef1a8762fb396fdfa29a9cb057ef72f4ce37821a
Eamon Heffernan <eamon.heffernan@finplex.com.au>
2022-12-31 16:29:57 +1100

    Back to window.location.host

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,15 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import f from '$env/static/public';
 	import { supabase } from '$lib/supabaseClient';
-
-	const getURL = () => {
-		console.log(f);
-
-		let url =
-			f.PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-			f.PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-			'http://localhost:4173/';
-		// Make sure to include `https://` when not localhost.
-		url = url.includes('http') ? url : `https://${url}`;
-		// Make sure to including trailing `/`.
-		url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
-
-		console.log(url);
-		return url;
-	};
 
 	const signInWithDiscord = async () => {
 		try {
+			console.log('test');
 			console.log(window.location.origin);
 			const { data, error } = await supabase.auth.signInWithOAuth({
 				provider: 'discord',
 				options: {
-					redirectTo: getURL()
+					redirectTo: window.location.host
 				}
 			});
 		} catch (error) {
